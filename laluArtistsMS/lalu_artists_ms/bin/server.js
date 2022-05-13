83ee019aeb051654efbdd157672ba0b2ba55f713
mrodrigueztr <mrodrigueztr@unal.edu.co>
2022-05-13 00:18:28 -0500

    Change port to do testing in localhost

--- laluArtistsMS/lalu_artists_ms/bin/server.js
+++ laluArtistsMS/lalu_artists_ms/bin/server.js
@@ -1,7 +1,7 @@
 const connectDB = require("../db/lalu_artist_db");
 const app = require("../laluArtist");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 const start = async () => {
   try {
